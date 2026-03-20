9cf2a179be22405bd74beba7ed98238736e154c2
Coppola Gionatan <coppolag@pascal.local>
2026-03-20 09:41:27 +0100

    Classi

---
+++ Prj_Animali/src/prj_animali/Pappagallo.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prj_animali;
+
+/**
+ *
+ * @author coppolag
+ */
+public class Pappagallo extends Animale {
+    private boolean parla;
+
+    public Pappagallo(String nome, int eta, boolean parla) {
+        super(nome, eta);
+        this.parla = parla;
+    }
+
+    public boolean isParla() {
+        return parla;
+    }
+
+    @Override
+    public String verso() {
+        return "Squawk";
+    }
+
+    @Override
+    public String descrizione() {
+        return "Pappagallo: " + getNome() +
+               ", Età: " + getEta() +
+               ", Parla: " + (parla ? "Sì" : "No") +
+               ", Verso: " + verso();
+    }
+
+    // Metodo specifico
+    public void ripetiFrase(String frase) {
+        if (parla) {
+            System.out.println(getNome() + " dice: " + frase + " 🦜");
+        } else {
+            System.out.println(getNome() + " non sa parlare.");
+        }
+    }
+}
---
+++ Prj_Animali/src/prj_animali/Pesce.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prj_animali;
+
+/**
+ *
+ * @author coppolag
+ */
+public class Pesce extends Animale {
+    private String acqua; // dolce o salata
+
+    public Pesce(String nome, int eta, String acqua) {
+        super(nome, eta);
+        this.acqua = acqua;
+    }
+
+    public String getAcqua() {
+        return acqua;
+    }
+
+    @Override
+    public String verso() {
+        return "..."; // i pesci non fanno un vero verso
+    }
+
+    @Override
+    public String descrizione() {
+        return "Pesce: " + getNome() +
+               ", Età: " + getEta() +
+               ", Acqua: " + acqua +
+               ", Verso: " + verso();
+    }
+
+    // Metodo specifico
+    public void nuota() {
+        System.out.println(getNome() + " sta nuotando 🐟");
+    }
+}
